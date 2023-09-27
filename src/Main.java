c1ef28ff9419185daeef95c362cf58c81583afa0
LandonTopy <landontopy@gmail.com>
2023-09-27 09:36:28 -0400

    :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String party;
+
+        System.out.println("What is your party affiliation? (D, R, I)");
+        party = scan.next();
+        if (party.equals("D")) {
+            System.out.println("You get a Democratic Donkey");
+        }else if (party.equals("R")) {
+            System.out.println("You get a Republican Elephant");
+        } else if (party.equals("I")) {
+            System.out.println("You get an Independent Person");
+        } else
+            System.out.println("Your party is other");
+        }
     }
-}
